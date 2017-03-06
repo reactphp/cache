92f264583f0238cf09326b30655d7aaf0d02f74c
Maciej Mroziński <maciej.mrozinski@kinguin.net>
2017-03-06 15:38:32 +0100

    Remove params doc blocks

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -7,20 +7,16 @@ use React\Promise\PromiseInterface;
 interface CacheInterface
 {
     /**
-     * @param $key
      * @return PromiseInterface
      */
     public function get($key);
 
     /**
-     * @param $key
-     * @param $value
      * @return PromiseInterface
      */
     public function set($key, $value);
 
     /**
-     * @param $key
      * @return PromiseInterface
      */
     public function remove($key);
