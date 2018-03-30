65e95c28179666b7d690425230bb00131941f60b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-03-30 07:48:39 +0200

    TTL skeleton with expiration queueing using SplPriorityQueue

--- src/ArrayCache.php
+++ src/ArrayCache.php
@@ -3,11 +3,18 @@
 namespace React\Cache;
 
 use React\Promise;
+use SplPriorityQueue;
 
 class ArrayCache implements CacheInterface
 {
     private $limit;
     private $data = array();
+    private $expires = array();
+
+    /**
+     * @var SplPriorityQueue
+     */
+    private $expiresQueue;
 
     /**
      * The `ArrayCache` provides an in-memory implementation of the [`CacheInterface`](#cacheinterface).
@@ -39,10 +46,16 @@ class ArrayCache implements CacheInterface
     public function __construct($limit = null)
     {
         $this->limit = $limit;
+        $this->expiresQueue = new SplPriorityQueue();
+        $this->expiresQueue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
     }
 
     public function get($key, $default = null)
     {
+        if (array_key_exists($key, $this->expires)) {
+            $this->garbageCollection();
+        }
+
         if (!array_key_exists($key, $this->data)) {
             return Promise\resolve($default);
         }
@@ -51,28 +64,55 @@ class ArrayCache implements CacheInterface
         $value = $this->data[$key];
         unset($this->data[$key]);
         $this->data[$key] = $value;
-
         return Promise\resolve($value);
     }
 
-    public function set($key, $value)
+    public function set($key, $value, $ttl = null)
     {
+        $expires = null;
+
+        if (is_int($ttl)) {
+            $this->expires[$key] = microtime(true) + $ttl;
+            $this->expiresQueue->insert($key, 0 - $this->expires[$key]);
+        }
+
         // unset before setting to ensure this entry will be added to end of array
         unset($this->data[$key]);
         $this->data[$key] = $value;
 
-        // ensure size limit is not exceeded or remove first entry from array
-        if ($this->limit !== null && count($this->data) > $this->limit) {
-            reset($this->data);
-            unset($this->data[key($this->data)]);
-        }
+        $this->garbageCollection();
 
         return Promise\resolve(true);
     }
 
     public function remove($key)
     {
-        unset($this->data[$key]);
+        unset($this->data[$key], $this->expires[$key]);
+        $this->garbageCollection();
         return Promise\resolve(true);
+    }
+
+    private function garbageCollection()
+    {
+        // ensure size limit is not exceeded or remove first entry from array
+        while ($this->limit !== null && count($this->data) > $this->limit) {
+            reset($this->data);
+            unset($this->data[key($this->data)]);
+        }
+
+        if ($this->expiresQueue->count() === 0) {
+            return;
+        }
+
+        $this->expiresQueue->rewind();
+        do {
+            $run = false;
+            $item = $this->expiresQueue->current();
+            if ((int)substr((string)$item['priority'], 1) <= microtime(true)) {
+                $this->expiresQueue->extract();
+                $run = true;
+                unset($this->data[$item['data']], $this->expires[$item['data']]);
+            }
+        } while ($run && $this->expiresQueue->count() > 0);
     }
 }
